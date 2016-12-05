53c5ea7d5eb4c001cc6cb0cd830d5c524305429c
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-12-05 15:54:11 -0800

    change UI Layout, embed in view controller

---
+++ MockSolo/MSADraftPlayersViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MSADraftPlayersViewController.swift
+//  MockSolo
+//
+//  Created by Josiah Mory on 12/5/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import UIKit
+
+class MSADraftPlayersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  // MARK: - Memory Warning
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
