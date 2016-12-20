59fa6e13adfb9d4e1ec94e39a7aa3f80b0017747
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-12-19 18:12:41 -0800

    add new model for roster

---
+++ MockSolo/Roster.swift
@@ -0,0 +1,17 @@
+//
+//  Roster.swift
+//  MockSolo
+//
+//  Created by Josiah Mory on 12/19/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import Foundation
+
+class Roster {
+  var rosterPositions: [String: String]
+
+  init(_ roster: [String: String]) {
+    self.rosterPositions = roster
+  }
+}
