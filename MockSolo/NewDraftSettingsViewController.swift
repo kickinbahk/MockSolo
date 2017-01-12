554edd80dc554a4b7bcd47ee041b46f4c4bf645a
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-01-12 08:01:41 -0800

    add new controller for settings

---
+++ MockSolo/NewDraftSettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NewDraftSettingsViewController.swift
+//  MockSolo
+//
+//  Created by Josiah Mory on 1/12/17.
+//  Copyright © 2017 kickinbahk Productions. All rights reserved.
+//
+
+import UIKit
+
+class NewDraftSettingsViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+}
