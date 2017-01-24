9f46e0a3bb89aa40590b6c97647cc6d7520f5ce9
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-01-23 18:41:40 -0800

    further adjust player removal algorithm

--- MockSolo/Draft.swift
+++ MockSolo/Draft.swift
@@ -182,13 +182,13 @@ class Draft {
     if count % 2 == 0 {
       let amountToRemove = (draftPickNumber * 2) - 1
       print("number to remove: \(amountToRemove)")
-      for num in 0...amountToRemove {
+      for _ in 0..<amountToRemove {
         players.remove(at: 0)
       }
     } else if count % 2 != 0 {
-      let amountToRemove = ((numberOfDrafters - draftPickNumber) * 2) - 1
+      let amountToRemove = ((numberOfDrafters - draftPickNumber) * 2)
       print(amountToRemove)
-      for num in 0...amountToRemove {
+      for _ in 0...amountToRemove {
         players.remove(at: 0)
       }
     }
