c45305b1bb9b8528ec0bf356e40c9dbc521e2992
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-01-24 07:55:26 -0800

    add test for different starting position

--- MockSoloTests/MockSoloTests.swift
+++ MockSoloTests/MockSoloTests.swift
@@ -30,13 +30,47 @@ class MockSoloTests: XCTestCase {
   func testRosterHasValues() {
     XCTAssertNotNil(roster)
   }
+  
   func testStartDraftRemovesCorrectNumberOfPlayers() {
-    let position = DraftManager.sharedInstance.draftPickNumber
-    DraftManager.sharedInstance.startDraft()
-    let startingPlayer = DraftManager.sharedInstance.players[0]
+    let draft = DraftManager.sharedInstance
+    let position = draft.draftPickNumber
+    draft.startDraft()
+    let startingPlayer = draft.players[0]
     
     XCTAssertEqual(position, startingPlayer.rank)
   }
   
+  func testGivenNonDefaultNumberStartDraftRemovesCorrectNumberOfPlayers() {
+    let draft = DraftManager.sharedInstance
+    draft.draftPickNumber = 2
+    let position = draft.draftPickNumber
+    draft.startDraft()
+    let startingPlayer = draft.players[0]
+    
+    XCTAssertEqual(position, startingPlayer.rank)
+  }
+  
+  func testCorrectNumberofPlayersAreRemovedFrom5SpotWith10Teams() {
+    DraftManager.sharedInstance.draftPickNumber = 5
+    DraftManager.sharedInstance.numberOfDrafters = 10
+    let secondPickPosition = 16
+    DraftManager.sharedInstance.startDraft()
+    DraftManager.sharedInstance.removePreviousPlayers()
+    let secondPick = DraftManager.sharedInstance.players[0]
+    
+    XCTAssertEqual(secondPickPosition, secondPick.rank)
+  }
+  
+  func testCorrectNumberofPlayersAreRemovedFrom1SpotWith10Teams() {
+    DraftManager.sharedInstance.draftPickNumber = 1
+    DraftManager.sharedInstance.numberOfDrafters = 10
+    let secondPickPosition = 20
+    DraftManager.sharedInstance.startDraft()
+    DraftManager.sharedInstance.removePreviousPlayers()
+    let secondPick = DraftManager.sharedInstance.players[0]
+    
+    XCTAssertEqual(secondPickPosition, secondPick.rank)
+    
+  }
   
 }
