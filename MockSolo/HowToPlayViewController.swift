c11f7cd8d2aceb56b76d1e729e764feceeebcdd1
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-02-08 08:47:23 -0800

    add close button and constraints

--- MockSolo/HowToPlayViewController.swift
+++ MockSolo/HowToPlayViewController.swift
@@ -17,13 +17,23 @@ class HowToPlayViewController: UIViewController {
     super.viewDidLoad()
 
     updateUI()
+    view.layoutIfNeeded()
   }
   
   func updateUI() {
     let image = UIImage(named: "stadium")
     backgroundImage = UIImageView(image: image!)
+  
+    closeButton.backgroundColor = UIColor(red: 46/255, green: 60/255, blue: 70/255, alpha: 1)
+    closeButton.layer.cornerRadius = 5
+    closeButton.contentEdgeInsets = UIEdgeInsetsMake(20, 20, 20, 20)
+    closeButton.setTitle("Close", for: .normal)
+    closeButton.setTitleColor(UIColor.white, for: .normal)
+    closeButton.addTarget(self, action: #selector(close), for: .touchUpInside)
+    
     
     view.addSubview(backgroundImage)
+    view.addSubview(closeButton)
     
     addConstraints()
   }
@@ -35,8 +45,14 @@ class HowToPlayViewController: UIViewController {
     backgroundImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
     backgroundImage.contentMode = .scaleAspectFill
     
+    closeButton.translatesAutoresizingMaskIntoConstraints = false
+    closeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+    closeButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -35)
+    
   }
 
-  
+  func close() {
+    dismiss(animated: true, completion: nil)
+  }
 
 }
