6c4a918d9ae0ccaef3cc4374274d37b03119805e
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-02-08 08:26:54 -0800

    add background image to HowToPlay and constraints

--- MockSolo/HowToPlayViewController.swift
+++ MockSolo/HowToPlayViewController.swift
@@ -10,18 +10,32 @@ import UIKit
 
 class HowToPlayViewController: UIViewController {
 
-  let backgroundImage = UIImage()
+  var backgroundImage = UIImageView()
   let closeButton = UIButton()
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    updateUI()
   }
   
   func updateUI() {
+    let image = UIImage(named: "stadium")
+    backgroundImage = UIImageView(image: image!)
     
+    view.addSubview(backgroundImage)
+    
+    addConstraints()
   }
 
+  func addConstraints() {
+    backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+    backgroundImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+    backgroundImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+    backgroundImage.contentMode = .scaleAspectFill
+    
+  }
 
   
 
