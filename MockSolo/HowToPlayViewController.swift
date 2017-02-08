dbad73ae419929cd67543d444683c869c28650c5
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-02-08 08:15:55 -0800

    add new view controller for howToPlay

---
+++ MockSolo/HowToPlayViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HowToPlayViewController.swift
+//  MockSolo
+//
+//  Created by Josiah Mory on 2/8/17.
+//  Copyright © 2017 kickinbahk Productions. All rights reserved.
+//
+
+import UIKit
+
+class HowToPlayViewController: UIViewController {
+
+  let backgroundImage = UIImage()
+  let closeButton = UIButton()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+  
+  func updateUI() {
+    
+  }
+
+
+  
+
+}
