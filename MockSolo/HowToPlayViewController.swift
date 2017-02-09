a105fe133a6a20baf51bd33080d238df56708ddc
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-02-08 17:56:49 -0800

    add text for instructions

--- MockSolo/HowToPlayViewController.swift
+++ MockSolo/HowToPlayViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class HowToPlayViewController: UIViewController {
   
   let closeButton = UIButton()
+  let instructionLabel = UILabel()
   
   var backgroundImage = UIImageView()
   var dimmedView = UIView()
+  
+  let instructions = "This app allows you to practice drafting on your own. Each pick you will only be able to select the available players below your current pick. It will help you get a feel of where you should target certain positions and where you can wait on a player. At the end of the draft you have constructed the team from the best available players with none falling to you. In the actual draft you should do even better as players will fall to you."
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -26,6 +29,10 @@ class HowToPlayViewController: UIViewController {
     let image = UIImage(named: "stadium")
     dimmedView.backgroundColor = UIColor.black.withAlphaComponent(0.65)
     backgroundImage = UIImageView(image: image!)
+    
+    instructionLabel.backgroundColor = UIColor.yellow
+    instructionLabel.text = instructions
+    instructionLabel.textColor = UIColor.white
   
     closeButton.backgroundColor = UIColor(red: 46/255, green: 60/255, blue: 70/255, alpha: 1)
     closeButton.layer.cornerRadius = 5
@@ -37,6 +44,7 @@ class HowToPlayViewController: UIViewController {
     
     view.addSubview(backgroundImage)
     view.addSubview(dimmedView)
+    dimmedView.addSubview(instructionLabel)
     dimmedView.addSubview(closeButton)
     
     addConstraints()
@@ -55,9 +63,16 @@ class HowToPlayViewController: UIViewController {
     dimmedView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
     dimmedView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
     
+    instructionLabel.translatesAutoresizingMaskIntoConstraints = false
+    instructionLabel.topAnchor.constraint(equalTo: dimmedView.topAnchor, constant: 20).isActive = true
+    instructionLabel.leadingAnchor.constraint(equalTo: dimmedView.leadingAnchor, constant: 15).isActive = true
+    instructionLabel.trailingAnchor.constraint(equalTo: dimmedView.trailingAnchor, constant: -15).isActive = true
+    instructionLabel.bottomAnchor.constraint(equalTo: closeButton.topAnchor, constant: -20).isActive = true
+  
+    
     closeButton.translatesAutoresizingMaskIntoConstraints = false
     closeButton.centerXAnchor.constraint(equalTo: dimmedView.centerXAnchor).isActive = true
-    closeButton.bottomAnchor.constraint(equalTo: dimmedView.bottomAnchor, constant: -35).isActive = true
+    closeButton.bottomAnchor.constraint(equalTo: dimmedView.bottomAnchor, constant: -25).isActive = true
     
   }
 
