310e0fd84a99217dabacfc1e71ae0c5c2438e81d
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-02-11 14:20:33 -0800

    add image to background of restart draft

--- MockSolo/RestartDraftViewController.swift
+++ MockSolo/RestartDraftViewController.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class RestartDraftViewController: UIViewController {
+  let restartDraftLabel = UILabel()
+  let restartDraftButton = UIButton()
+  
+  var backgroundImage = UIImageView()
+  var dimmedView = UIView()
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    
+    updateUI()
+    view.layoutIfNeeded()
   }
 
+  func updateUI() {
+    let image = UIImage(named: "stadium_seats")
+    dimmedView.backgroundColor = UIColor.black.withAlphaComponent(0.65)
+    backgroundImage = UIImageView(image: image)
+    
+    view.addSubview(backgroundImage)
+    view.addSubview(dimmedView)
+    
+    addContstraints()
+  }
+  
+  func addContstraints() {
+    backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+    backgroundImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+    backgroundImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+    backgroundImage.contentMode = .scaleAspectFit
+    
+  }
 }
