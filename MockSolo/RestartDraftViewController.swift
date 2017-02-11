196a24475ef60b34a860a8dcf29f83b38d31803b
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-02-11 14:49:17 -0800

    add button to restartDraft view

--- MockSolo/RestartDraftViewController.swift
+++ MockSolo/RestartDraftViewController.swift
@@ -27,8 +27,17 @@ class RestartDraftViewController: UIViewController {
     dimmedView.backgroundColor = UIColor.black.withAlphaComponent(0.65)
     backgroundImage = UIImageView(image: image)
     
+    restartDraftButton.backgroundColor = UIColor(red: 46/255, green: 60/255, blue: 70/255, alpha: 1)
+    restartDraftButton.layer.cornerRadius = 5
+    restartDraftButton.contentEdgeInsets = UIEdgeInsetsMake(20, 20, 20, 20)
+    restartDraftButton.setTitle("Do you Want to Restart Your Draft?", for: .normal)
+    restartDraftButton.tintColor = UIColor.black
+    restartDraftButton.setTitleColor(UIColor.white, for: .normal)
+    restartDraftButton.addTarget(self, action: #selector(restartDraft), for: .touchUpInside)
+    
     view.addSubview(backgroundImage)
     view.addSubview(dimmedView)
+    dimmedView.addSubview(restartDraftButton)
     
     addContstraints()
   }
@@ -46,5 +55,13 @@ class RestartDraftViewController: UIViewController {
     dimmedView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
     dimmedView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
     
+    restartDraftButton.translatesAutoresizingMaskIntoConstraints = false
+    restartDraftButton.centerXAnchor.constraint(equalTo: dimmedView.centerXAnchor).isActive = true
+    restartDraftButton.bottomAnchor.constraint(equalTo: dimmedView.bottomAnchor, constant: -35).isActive = true
+    
+  }
+  
+  func restartDraft(_ sender: UIButton) {
+    performSegue(withIdentifier: "RestartDraft", sender: sender)
   }
 }
