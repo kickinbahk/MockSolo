59aa3be22d284b1e125478575b409e3b6c8d93c4
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-02-11 13:59:03 -0800

    add controller for restartd draft scene

---
+++ MockSolo/RestartDraftViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RestartDraftViewController.swift
+//  MockSolo
+//
+//  Created by Josiah Mory on 2/11/17.
+//  Copyright © 2017 kickinbahk Productions. All rights reserved.
+//
+
+import UIKit
+
+class RestartDraftViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    
+  }
+
+}
